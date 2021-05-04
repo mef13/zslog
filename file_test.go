156aae98f366d35eb3037d711de07cdcd3aac96a
mef <mef4mail@gmail.com>
2021-05-04 17:00:02 +0300

    file_test

---
+++ file_test.go
@@ -0,0 +1,24 @@
+package zslog
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestFileWriter(t *testing.T) {
+	filePath := filepath.Join(t.TempDir(), "testFile")
+	log := New(File(filePath, FileConfig{}, NewLevels()))
+	SetTimeFieldFormat("2006")
+	log.Trace().Msg("msg")
+	log.Close()
+	f, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := string(f), fmt.Sprintf("{\"level\":\"trace\",\"time\":\"%s\",\"message\":\"msg\"}\n", time.Now().Format("2006")); got != want {
+		t.Errorf("\ngot:\n%s\nwant:\n%s", got, want)
+	}
+}
